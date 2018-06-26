63d6c217bc3d24ca0218d37453555735973c3425
TamNguyen <huutam.ptnk@gmail.com>
2018-06-27 03:28:47 +0700

    second commit

--- get_image.py
+++ get_image.py
@@ -1,11 +1,19 @@
 from tkinter import *
 from screeninfo import get_monitors
 from PIL import ImageTk, Image
+
 root = Tk()
 
 photo = ImageTk.PhotoImage(Image.open("a.jpg"))
 #photo = PhotoImage(file = "a.jpg")
 label = Label(root, image=photo)
 label.pack(side = "bottom", fill = "both", expand = "yes")
-root.geometry("1536x864+0+0")
+
+# Set the app screen resolution
+# We need the screen resolution so that we scale the input pictures
+screen_width = get_monitors()[0].width
+screen_height = get_monitors()[0].height
+app_resolution = ""+str(screen_width)+"x"+str(screen_height)+"+0+0"
+
+root.geometry(app_resolution)
 root.mainloop()
